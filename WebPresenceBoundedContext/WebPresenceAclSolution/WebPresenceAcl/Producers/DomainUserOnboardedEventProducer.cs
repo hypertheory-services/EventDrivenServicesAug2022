29b715ff0c7b59ccefffd1fe5ee35acd15e2348a
Jeff Gonzalez <jeff@hypertheory.com>
2022-08-18 16:26:21 -0400

    added producer

---
+++ WebPresenceBoundedContext/WebPresenceAclSolution/WebPresenceAcl/Producers/DomainUserOnboardedEventProducer.cs
@@ -0,0 +1,13 @@
+﻿using Hypertheory.KafkaUtils.Producers;
+using Hypertheory.Events;
+using Confluent.SchemaRegistry.Serdes;
+using Hypertheory.KafkaUtils.Handlers;
+
+namespace WebPresenceAcl.Producers;
+
+public class DomainUserOnboardedEventProducer : EventProducer<UserOnboarded, ProtobufSerializer<UserOnboarded>>
+{
+    public DomainUserOnboardedEventProducer(ClientHandle handler, string registryUrl) : base(handler, registryUrl)
+    {
+    }
+}
