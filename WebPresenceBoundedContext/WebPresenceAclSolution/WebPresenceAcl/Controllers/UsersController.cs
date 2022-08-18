d78c4afeb7d7055d68eb75c193d70ecb24d39515
Jeff Gonzalez <jeff@hypertheory.com>
2022-08-18 16:03:18 -0400

    got the api started

---
+++ WebPresenceBoundedContext/WebPresenceAclSolution/WebPresenceAcl/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPresenceAcl.Controllers;
+
+[ApiController]
+public class UsersController : ControllerBase
+{
+
+    // when there is an internal new user created, publish it as a domain user onboarded
+
+    [HttpPost]
+    [Route("/webpresence/acl/map-new-user-to-domain")]
+    public async Task<ActionResult> MapOnboard()
+    {
+        return BadRequest();
+    }
+}
