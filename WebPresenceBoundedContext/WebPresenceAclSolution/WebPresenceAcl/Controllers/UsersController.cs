593c157a95ee750d8e46c32f9199b6fedf2ea23d
Jeff Gonzalez <jeff@hypertheory.com>
2022-08-18 16:31:59 -0400

    controller looks ready

--- WebPresenceBoundedContext/WebPresenceAclSolution/WebPresenceAcl/Controllers/UsersController.cs
+++ WebPresenceBoundedContext/WebPresenceAclSolution/WebPresenceAcl/Controllers/UsersController.cs
@@ -1,6 +1,12 @@
-﻿using Dapr;
+﻿using Confluent.Kafka;
+
+using Dapr;
+
+using Hypertheory.Events;
 
 using Microsoft.AspNetCore.Mvc;
+
+using WebPresenceAcl.Producers;
 
 using WebPresenceMessages.Users;
 
@@ -10,6 +16,13 @@ namespace WebPresenceAcl.Controllers;
 public class UsersController : ControllerBase
 {
 
+    private readonly DomainUserOnboardedEventProducer _producer;
+
+    public UsersController(DomainUserOnboardedEventProducer producer)
+    {
+        _producer = producer;
+    }
+
     // when there is an internal new user created, publish it as a domain user onboarded
 
     [Topic("webpresence-acl-dev", "webpresence-internal-user-created")]
@@ -17,7 +30,23 @@ public class UsersController : ControllerBase
     [Route("/webpresence/acl/map-new-user-to-domain")]
     public async Task<ActionResult> MapOnboard(NewUserCreated request)
     {
-        
+        var messageToPublish = new Hypertheory.Events.UserOnboarded
+        {
+           Email = request.Email,
+           FirstName = request.FirstName,
+           LastName = request.LastName,
+           Source = "web-presence",
+           UserId = request.UserId,
+        };
+        // Step 2???
+        var message = new Message<Null, UserOnboarded>
+        {
+            Value = messageToPublish
+        };
+
+        await _producer.ProduceAsync("hypertheory-events-useronboarded", message);
+
+        // Step 3 Profit!
         return BadRequest();
     }
 }
--- WebPresenceBoundedContext/WebPresenceAclSolution/WebPresenceAcl/Program.cs
+++ WebPresenceBoundedContext/WebPresenceAclSolution/WebPresenceAcl/Program.cs
@@ -1,15 +1,38 @@
+using Confluent.Kafka;
+using WebPresenceAcl.Producers;
+
+using Hypertheory.KafkaUtils.Handlers;
+
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddOptions<ProducerConfig>().Bind(builder.Configuration.GetSection("Kafka:ProducerSettings"));
+var schemaUrl = builder.Configuration.GetConnectionString("schema-registry");
+
+builder.Services.AddSingleton<ClientHandle>(sp =>
+{
+    var config = sp.GetRequiredService<IOptions<ProducerConfig>>().Value;
+    return new ClientHandle(config);
+});
+
+builder.Services.AddSingleton<DomainUserOnboardedEventProducer>(sp =>
+{
+    var handle = sp.GetRequiredService<ClientHandle>();
+    return new DomainUserOnboardedEventProducer(handle, schemaUrl);
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.UseCloudEvents();
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapSubscribeHandler();
 
 app.Run();
